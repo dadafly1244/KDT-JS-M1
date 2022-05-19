09ae06838f23b1955c5c5319cc2211c22a77ffd0
YANGDAYOUNG <jc1525@hanmail.net>
2022-05-19 17:07:19 +0900

    fix: 배포문제해결시도

--- js/getapi.js
+++ js/getapi.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export async function fetchDataByTitle(title, currentPage = 1) {
   try {
     return await axios({
-      url: `https://www.omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${title}&page=${currentPage}`,
+      //url: `https://www.omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${title}&page=${currentPage}`,
+      url: `https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}&page=${currentPage}`,
       method: 'GET'
     })
   } catch(error) {
